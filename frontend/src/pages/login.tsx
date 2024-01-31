b0000ccebe5fed04f0c18db23d77e4743815b584
Davy <davyrobert90@gmail.com>
2024-01-31 21:13:34 +0100

    config: Config project

---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,7 @@
+const LoginPage = () => {
+   return (
+      <div>
+         <h1>Login Page !</h1>
+      </div>
+   )
+}
---
+++ frontend/src/pages/register.tsx
@@ -0,0 +1,7 @@
+const RegisterPage = () => {
+   return (
+      <div>
+         <h1>Register Page !</h1>
+      </div>
+   )
+}
