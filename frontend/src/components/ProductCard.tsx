8151ece64603960eb78e7e5a46156d2d97f74504
kjarret <jarretkenny@gmail.com>
2024-02-02 09:55:14 +0100

    Add Product components & delete routes

---
+++ frontend/src/components/ProductCard.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+//import styles from "../styles/ProductCard.module.css";
+
+export type ProductCardProps = {
+  id?: number;
+  name: string;
+  created_at?: Date;
+  price: number;
+  description: string;
+  quantity: Float32Array;
+  location: string;
+  category: {
+    id: number;
+    name: string;
+  };
+};
+
+const ProductCard = ({
+  name,
+  description,
+  price,
+  quantity,
+  location,
+}: ProductCardProps) => {
+  return (
+    <div>
+      <div>
+        {name}
+      </div>
+      <div>
+        {description}
+      </div>
+      <div>{price}</div>
+
+      <div>{quantity}</div>
+
+      <div>{location}</div>
+    </div>
+  );
+};
+
+export default ProductCard;
