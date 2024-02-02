fb7f1641bd70a7d1668ba6ea86705c9b6ab70abd
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-02-02 15:26:03 +0100

    feat(backend): :sparkles: update product

--- backend/src/entities/index.ts
+++ backend/src/entities/index.ts
@@ -1,9 +1,10 @@
-import { Product, InputCreateProduct } from './product.entity';
+import { Product, InputCreateProduct, InputUpdateProduct } from './product.entity';
 import { Category, InputCreateCategory } from './category.entity';
 
 export {
   Product,
   InputCreateProduct,
+  InputUpdateProduct,
   Category,
   InputCreateCategory
 };
