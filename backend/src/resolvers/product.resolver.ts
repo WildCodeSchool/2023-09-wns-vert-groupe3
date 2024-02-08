74b32533353546393b051382060c503165465bb5
Davy <davyrobert90@gmail.com>
2024-02-08 22:58:36 +0100

    fix: add products back

--- backend/src/resolvers/product.resolver.ts
+++ backend/src/resolvers/product.resolver.ts
@@ -1,5 +1,5 @@
 import ProductService from "../services/product.service";
-import { InputCreateProduct, InputUpdateProduct, Product } from "../entities";
+import { InputCreateProduct, /* InputUpdateProduct */ Product } from "../entities";
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
@@ -17,30 +17,37 @@ export default class ProductResolver {
   }
 
   @Mutation(() => Product)
-  async addProduct(@Arg("infos") infos: InputCreateProduct) {
-    const newProduct = await new ProductService().create(infos);
-    return newProduct;
+  async addProduct(
+   @Arg("infos") infos: InputCreateProduct) 
+   {
+   //  const newProduct = await new ProductService().create(infos);
+   //  return newProduct;
+
+   return await Product.save({
+      ...infos,
+      category: {id: infos.category}
+   })
   }
 
-  @Mutation(() => Product)
-  async updateProduct(
-    @Arg("id") id: number,
-    @Arg("infos") data: InputUpdateProduct) {
+//   @Mutation(() => Product)
+//   async updateProduct(
+//     @Arg("id") id: number,
+//     @Arg("infos") data: InputUpdateProduct) {
 
-    const productToUpdate = await new ProductService().update(id, { ...data });
-    console.log(productToUpdate);
-    return productToUpdate;
-  }
+//     const productToUpdate = await new ProductService().update(id, { ...data });
+//     console.log(productToUpdate);
+//     return productToUpdate;
+//   }
   
-  @Mutation(() => Boolean)
-  async deleteProduct(@Arg("productId") productId: number): Promise<boolean> {
-    try {
-      await new ProductService().deleteProduct(productId);
-      return true;
-    } catch (error) {
-      console.error(error);
-      return false;
-    }
-  }
+//   @Mutation(() => Boolean)
+//   async deleteProduct(@Arg("productId") productId: number): Promise<boolean> {
+//     try {
+//       await new ProductService().deleteProduct(productId);
+//       return true;
+//     } catch (error) {
+//       console.error(error);
+//       return false;
+//     }
+//   }
 
 }
