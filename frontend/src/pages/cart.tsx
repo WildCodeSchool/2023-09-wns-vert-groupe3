20d87d9819e9d74afcd97745242af0d98dcee3a8
Davy <davyrobert90@gmail.com>
2024-02-08 14:26:53 +0100

    chore: construction route and folder styles css

---
+++ frontend/src/pages/cart.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "../styles/pages/CartPage.module.scss"
+
+const CartPage = () => {
+   return (
+      <main className={styles.cartPage}>
+         <div>
+            <h2 className={styles.title}>Welcome to the cart page !</h2>
+         </div>
+      </main>
+   )
+}
+
+export default CartPage
