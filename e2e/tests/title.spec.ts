ccf46d2b9d1f4c75a87eb0dfd7ba412e3a088c8b
kjarret <jarretkenny@gmail.com>
2024-03-14 11:35:23 +0100

    fix(test-e2e): :bug: delete github/workflows in e2e and add some tests

--- e2e/tests/title.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from "@playwright/test";
-
-test("Find URL", async ({ page }) => {
-  await page.goto("http://frontend:3000/", { timeout: 60000 });
-});
-
-// cmd execute test : pnpm exec playwright test
