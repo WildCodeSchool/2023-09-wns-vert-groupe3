94df6f12b9131fd88b8cbecd30c33409db2995c3
Davy <davyrobert90@gmail.com>
2024-03-14 09:22:26 +0100

    feat: N30 - add modifications title.spec

--- e2e/tests/title.spec.ts
+++ e2e/tests/title.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "@playwright/test";
 
 test("Find URL", async ({ page }) => {
-  await page.goto("http://localhost:3000/", { timeout: 60000 });
+  await page.goto("http://frontend:3000/", { timeout: 60000 });
 });
 
 // cmd execute test : pnpm exec playwright test
