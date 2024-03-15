1aa9e734475c591c64fbc15a6b89ca1fe2009644
Axel P <piglow19@gmail.com>
2024-03-15 14:20:32 +0100

    feat: :sparkles: First iteration of the product details page

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['localhost', 'via.placeholder.com'],
+  },
+};
 
 module.exports = nextConfig;
---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, className, ...props }) => {
+  return <button className={cn("py-3 bg-primary text-white font-bold rounded-xl hover:bg-opacity-80 transition", className)} {...props}>{children}</button>;
+};
+export default Button;
