87a555d265563d99fc278360476718dc1318e59d
Davy <davyrobert90@gmail.com>
2024-03-15 12:25:40 +0100

    add port db docker compose

--- imagesupload/src/index.ts
+++ imagesupload/src/index.ts
@@ -8,49 +8,49 @@ const app = express();
 const port = 8000;
 app.use(cors());
 
-app.get("/", (_, res) => {
-  res.send("Hello World!");
+app.get("/", (_: any, res: any) => {
+   res.send("Hello World!");
 });
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "../uploads/"));
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
+   destination: function (_req: any, _file, cb) {
+      cb(null, path.join(__dirname, "../uploads/"));
+   },
+   filename: function (_req: any, file: any, cb) {
+      cb(null, Date.now() + "-" + file.originalname);
+   },
 });
 const upload = multer({ storage: storage });
 
 app.post("/upload", upload.single("file"), (req: any, res: Response) => {
-  fs.readFile(req.file.path, (err) => {
-    if (err) {
-      console.log("Error: ", err);
-      res.status(500).json({ error: err });
-    } else {
-      res
-        .status(201)
-        .json({ status: "success", filename: "/files/" + req.file.filename });
-    }
-  });
+   fs.readFile(req.file.path, (err) => {
+      if (err) {
+         console.log("Error: ", err);
+         res.status(500).json({ error: err });
+      } else {
+         res
+            .status(201)
+            .json({ status: "success", filename: "/files/" + req.file.filename });
+      }
+   });
 });
 
-app.get("/files/:filename", (req, res) => {
-  let file = path.join(__dirname + "/../uploads", req.params.filename);
-  console.log("file", file);
-  fs.readFile(file, (err, content) => {
-    if (err) {
-      res.writeHead(404, { "Content-Type": "text" });
-      res.write("File Not Found!");
-      res.end();
-    } else {
-      res.writeHead(200, { "Content-Type": "application/octet-stream" });
-      res.write(content);
-      res.end();
-    }
-  });
+app.get("/files/:filename", (req: any, res: any) => {
+   let file = path.join(__dirname + "/../uploads", req.params.filename);
+   console.log("file", file);
+   fs.readFile(file, (err, content) => {
+      if (err) {
+         res.writeHead(404, { "Content-Type": "text" });
+         res.write("File Not Found!");
+         res.end();
+      } else {
+         res.writeHead(200, { "Content-Type": "application/octet-stream" });
+         res.write(content);
+         res.end();
+      }
+   });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+   console.log(`Example app listening on port ${port}`);
 });
