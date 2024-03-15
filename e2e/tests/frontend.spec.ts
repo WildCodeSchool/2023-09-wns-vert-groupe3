58d39ed6ea9e2be279476885e7b5dfe3e0c5add3
kjarret <jarretkenny@gmail.com>
2024-03-15 09:46:56 +0100

    fix(test-e2e): :bug: change localhost to frontend

--- e2e/tests/frontend.spec.ts
+++ e2e/tests/frontend.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("Find URL and title", async ({ page }) => {
-  await page.goto("http://localhost:3000/", { timeout: 60000 });
+  await page.goto("http://frontend:3000/", { timeout: 60000 });
   await expect(
     page.getByRole("heading", {
       name: "Louez aujourd'hui, venez le récupérer dans l'heure",
@@ -10,9 +10,9 @@ test("Find URL and title", async ({ page }) => {
 });
 
 test("Find all pages", async ({ page }) => {
-  await page.goto("http://localhost:3000/products");
-  await page.goto("http://localhost:3000/cart");
-  await page.goto("http://localhost:3000/profile");
+  await page.goto("http://frontend:3000/products");
+  await page.goto("http://frontend:3000/cart");
+  await page.goto("http://frontend:3000/profile");
 });
 
 // cmd execute test : pnpm exec playwright test
