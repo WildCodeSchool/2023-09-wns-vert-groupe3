8214b3b4fd25ccea717d16ae704d8327411fb99f
Davy ROBERT <davyrobert90@gmail.com>
2024-05-17 09:09:55 +0200

    fix colors categories

--- frontend/src/pages/backoffice/productslist.tsx
+++ frontend/src/pages/backoffice/productslist.tsx
@@ -16,6 +16,28 @@ const productslist = () => {
    const [selectedArticle, setSelectedArticle] = useState<any>(null);
    const router = useRouter();
 
+   const getCategoryColor = (categoryName: string) => {
+
+      switch (categoryName) {
+         case "Ski":
+            return "bg-gradient-to-br from-sky-500 via-sky-500 to-indigo-500 text-white";
+         case "Plongée":
+            return "bg-gradient-to-br from-blue-700 via-blue-700 to-indigo-500 text-white";
+         case "Randonnée":
+            return "bg-gradient-to-br from-green-600 via-green-600 to-indigo-500 text-white";
+         case "Escalade":
+            return "bg-gradient-to-br from-amber-800 via-amber-800 to-indigo-500 text-white";
+         case "Camping":
+            return "bg-gradient-to-br from-yellow-600 via-yellow-600 to-indigo-500 text-white";
+         case "Rafting":
+            return "bg-gradient-to-br from-blue-700 via-blue-700 to-indigo-500 text-white";
+         case "Pêche":
+            return "bg-slate-500 text-white";
+         default:
+            return "bg-slate-500";
+      }
+   };
+
    const [deleteProduct, { loading: deleteProductLoading, error: deleteProductError }] = useMutation(DELETE_PRODUCT);
 
    const handleDelete = async (productId: string) => {
