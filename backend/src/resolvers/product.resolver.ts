1bde73bd10a2e15345591b38ca495556a04c39f1
kjarret <134682611+kjarret@users.noreply.github.com>
2024-05-17 10:59:26 +0200

    Feat/add backend cache with redis: 1502664284 (#56)
    * feat(backend): :sparkles: Add cache server Redis to backend
    
    * fix(backend): :bug: change legacymode on redis client
    
    * fix(test-e2e): :bug: Add redis to docker compose test
    
    * fix(backend): :bug: removing legacy mode to test

--- backend/src/resolvers/product.resolver.ts
+++ backend/src/resolvers/product.resolver.ts
@@ -1,4 +1,6 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Like } from "typeorm";
+import { redisClient } from "../../src/index";
 import { Product } from "../entities";
 import { InputCreateProduct, InputUpdateProduct } from "../inputs";
 import ProductService from "../services/product.service";
@@ -10,11 +12,19 @@ export default class ProductResolver {
     return await new ProductService().list();
   }
 
-  @Query(() => Product, { nullable: true })
-  async oneProductById(@Arg("id") id: number) {
-    const oneProduct = await new ProductService().findById(id);
-    console.log(oneProduct);
-    return oneProduct;
+  @Query(() => [Product])
+  async getAllProductsByKeyword(@Arg("keyword") keyword: string) {
+    const cacheResult = await redisClient.get(keyword);
+    if (cacheResult !== null) {
+      console.log("From cache");
+      return JSON.parse(cacheResult);
+    } else {
+      const dbResult = await Product.find({
+        where: { description: Like(`%${keyword}%`) },
+      });
+      redisClient.set(keyword, JSON.stringify(dbResult), { EX: 60 });
+      return dbResult;
+    }
   }
 
   @Query(() => [Product])
