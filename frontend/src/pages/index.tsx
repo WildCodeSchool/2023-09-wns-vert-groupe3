ae463a5b93e71fa6119ed1aeef213ee4d575f6f1
kjarret <jarretkenny@gmail.com>
2024-05-28 15:22:12 +0200

    feat(frontend): :sparkles: Add second section to main page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,13 @@
 import HomeHotProductsSection from "containers/public/home/hot-products-section";
 import HomeIntroSection from "containers/public/home/intro-section";
-
+import SummerProductsSection from "containers/public/home/summer-products";
 
 export default function Home() {
   return (
     <>
       <HomeIntroSection />
       <HomeHotProductsSection />
+      <SummerProductsSection />
     </>
   );
 }
