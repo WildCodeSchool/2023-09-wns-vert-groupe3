af1fab84576b3e4ec77e44bb8a5c6d17fb084afc
Davy ROBERT <davyrobert90@gmail.com>
2024-06-12 22:01:32 +0200

    change profil page

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -2,41 +2,22 @@ import { Context } from "@apollo/client";
 import { getSession, signOut, useSession } from "next-auth/react";
 import styles from "../styles/pages/ProfilePage.module.scss";
 
+export const getServerSideProps = () => {
+   return {
+     redirect: {
+       destination: "/login",
+     },
+   };
+};
+
 const ProfilePage = () => {
-  const { data: session, status } = useSession();
-
-  if (status === "authenticated") {
-    return (
+   return (
       <main className={styles.profilePage}>
-        <p>Bienvenue sur votre profile {session.user?.name}</p>
-        <button onClick={() => signOut()} className="underline">
-          Déconnexion
-        </button>
+         <p>Bienvenue sur votre profil </p>
+         <button className="underline">
+            Déconnexion
+         </button>
       </main>
-    );
-  } else {
-    return (
-      <div>
-        <p>Vous n&apos;êtes pas connecté</p>
-        <br />
-      </div>
-    );
-  }
-};
-
-export const getServerSideProps = async (context: Context) => {
-  const session = await getSession(context);
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/login",
-      },
-    };
-  }
-
-  return {
-    props: { session },
-  };
-};
-
+   );
+}
 export default ProfilePage;
