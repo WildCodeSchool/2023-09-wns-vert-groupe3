b16023b9d74f21ec8f02f41de8b4d38451af40f7
kjarret <jarretkenny@gmail.com>
2024-07-21 13:28:29 +0200

    feat(frontend): :sparkles: Add Skeleton on loading of product

--- frontend/src/containers/public/home/hot-products-section.tsx
+++ frontend/src/containers/public/home/hot-products-section.tsx
@@ -1,24 +1,49 @@
 import { useQuery } from "@apollo/client";
 import DisplayProd from "components/DisplayProd";
-import LoadingProgress from "components/ui/LoadingProgress";
 import { GET_PRODUCTS } from "lib/graphql/queries";
 import { HiPlusCircle } from "react-icons/hi";
-
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import styles from "styles/pages/ProductsPage.module.scss";
 
 const HomeHotProductsSection = () => {
   const { data, loading, error } = useQuery(GET_PRODUCTS);
 
-  if (loading) return <LoadingProgress />;
+  if (loading) {
+    return (
+      <section className="mt-32 flex flex-col gap-2">
+        <div className="fit-content mb-4 flex">
+          <HiPlusCircle className="mr-2 text-4xl text-blue-500" />
+          <h2 className="w-fit border-b-2 border-b-blue-500 text-2xl font-bold">
+            Les nouveautés
+          </h2>
+        </div>
+        <main className={styles.productsPage}>
+          <div className="grid grid-cols-2 gap-6 lg:grid-cols-2">
+            {[...Array(4)].map((_, index) => (
+              <SkeletonTheme
+                key={index}
+                baseColor="#202020"
+                highlightColor="#444"
+              >
+                <Skeleton height={300} width={600} />
+              </SkeletonTheme>
+            ))}
+          </div>
+        </main>
+      </section>
+    );
+  }
+
   if (error) return <p>Error: {error.message}</p>;
 
   const products = data.getAllproducts;
 
   return (
     <section className="mt-32 flex flex-col gap-2">
-      <div className="justifiy-center fit-content mb-4 flex">
+      <div className="fit-content mb-4 flex">
         <HiPlusCircle className="mr-2 text-4xl text-blue-500" />
-        <h2 className="w-fit items-center border-b-2 border-b-blue-500 text-2xl font-bold">
+        <h2 className="w-fit border-b-2 border-b-blue-500 text-2xl font-bold">
           Les nouveautés
         </h2>
       </div>
