32e22c4b3badc32d51ac0d7daedfc7ea28b10870
Davy ROBERT <davyrobert90@gmail.com>
2024-07-23 14:05:10 +0200

    arrange frontend.spec.ts and comment login.spec.ts

--- e2e/tests/frontend.spec.ts
+++ e2e/tests/frontend.spec.ts
@@ -1,24 +1,38 @@
 import { expect, test } from "@playwright/test";
 
 test("Find URL and title", async ({ page }) => {
-  await page.goto("http://frontend:3000/", { timeout: 60000 });
-  //   await page.goto("http://localhost:3000/", { timeout: 60000 });
+   await page.goto("http://frontend:3000/", { timeout: 60000 });
+   //   await page.goto("http://localhost:3000/", { timeout: 60000 });
 
-  await page.pause();
-  await expect(
-    page.getByRole("heading", {
-      name: "Louez aujourd'hui, venez le récupérer dans l'heure",
-    })
-  ).toBeVisible();
+   await page.pause();
+   await expect(
+      page.getByRole("heading", {
+         name: "Louez aujourd'hui, venez le récupérer dans l'heure",
+      })
+   ).toBeVisible();
 });
 
 test("Find all pages", async ({ page }) => {
-  await page.goto("http://frontend:3000/products");
-  //   await page.goto("http://frontend:3000/cart");
-  await page.goto("http://frontend:3000/login");
-  //   await page.goto("http://localhost:3000/products");
-  //   await page.goto("http://localhost:3000/cart");
-  //   await page.goto("http://localhost:3000/login");
+   await page.goto("http://frontend:3000/products");
+   await page.goto("http://frontend:3000/products/1");
+   await page.goto("http://frontend:3000/login");
+
+   // await page.goto("http://localhost:3000/products");
+   // await page.goto("http://localhost:3000/products/1");
+   // await page.goto("http://localhost:3000/login");
+});
+
+test('should load login page and display all elements', async ({ page }) => {
+   await page.goto('http://frontend:3000/login');
+   // await page.goto('http://localhost:3000/login');
+
+   await page.waitForLoadState('networkidle');
+   await expect(page.getByRole('heading', { name: 'Se connecter à votre compte' })).toBeVisible();
+   await expect(page.locator('input[name="email"]')).toBeVisible();
+   await expect(page.locator('input[name="password"]')).toBeVisible();
+   await expect(page.getByRole('button', { name: 'Se connecter' })).toBeVisible();
+   await expect(page.locator('input[type="checkbox"][id="remember"]')).toBeVisible();
+   await expect(page.getByRole('link', { name: "S'enregistrer" })).toBeVisible();
 });
 
 // cmd execute test : pnpm exec playwright test
