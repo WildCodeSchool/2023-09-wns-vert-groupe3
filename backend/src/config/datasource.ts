bfeee6128c435e816dc57163e947041d24cc9d48
Davy ROBERT <davyrobert90@gmail.com>
2024-07-25 18:56:17 +0200

    Configuration initial migration & add Makefle for Docker commands

--- backend/src/config/datasource.ts
+++ backend/src/config/datasource.ts
@@ -2,14 +2,19 @@ import { DataSource } from "typeorm";
 
 const dataSource = new DataSource({
   type: "postgres",
+  //   url: "postgres://postgres:wildrent@db:5432/postgres",
   host: "db",
+  //   host: "localhost",
   port: 5432,
+  //   port: 5434,
   username: "postgres",
   password: "wildrent",
   database: "postgres",
   entities: ["src/entities/*.ts"],
   synchronize: true,
   logging: ["error", "query"],
+  migrations: [__dirname + "/migrations/*.{js, ts}"], // Déterminer à quel endroit on enregistre les fichiers de migrations afin de pouvoir les relire.
+  migrationsRun: true // Assure l'application des migrations non encore appliquées à chaque connexion à la base de données.
 });
 
 export default dataSource;
