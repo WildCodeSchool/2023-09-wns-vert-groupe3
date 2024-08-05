cf5ca8e9d98e33843804e049c59b931a1bdd9c2a
kjarret <jarretkenny@gmail.com>
2024-08-05 11:54:22 +0200

    fix(many): :thread: Modify Node ENV in docker-compose file and fix urls in stripe and imageUpload

--- backend/src/resolvers/checkout.resolver.ts
+++ backend/src/resolvers/checkout.resolver.ts
@@ -1,6 +1,7 @@
 import { Arg, Mutation, Resolver } from "type-graphql";
 import { stripe } from "../index";
 import { InputCartProduct } from "../inputs";
+require("dotenv").config();
 
 @Resolver()
 export default class CheckoutResolver {
@@ -8,6 +9,16 @@ export default class CheckoutResolver {
   async createCheckoutSession(
     @Arg("products", () => [InputCartProduct]) products: InputCartProduct[]
   ) {
+    const successUrl =
+      process.env.NODE_ENV === "production"
+        ? `${process.env.URL_PROD_DEV}/stripe/success`
+        : `${process.env.NEXT_PUBLIC_FRONTEND_URL}/stripe/success`;
+
+    const cancelUrl =
+      process.env.NODE_ENV === "production"
+        ? `${process.env.URL_PROD_DEV}/cart`
+        : `${process.env.NEXT_PUBLIC_FRONTEND_URL}/cart`;
+
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items: products.map((product) => ({
@@ -27,10 +38,10 @@ export default class CheckoutResolver {
         quantity: product.quantity,
       })),
       mode: "payment",
-      success_url: "http://localhost:3000/stripe/success",
+      success_url: successUrl,
       /* success_url:
         "http://localhost:3000/stripe/success?session_id={CHECKOUT_SESSION_ID}", */
-      cancel_url: "http://localhost:3000/cart",
+      cancel_url: cancelUrl,
     });
     return session.url;
   }
